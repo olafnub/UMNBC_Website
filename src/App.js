34464fe489744b70760f705162467f248da7633f
MyManAy <66086069+MyManAy@users.noreply.github.com>
2023-12-06 17:34:50 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import './App.css';
-import {useState, useEffect} from 'react';
-import Navbar from './components/Navbar.js';
-import grouppic from './images/lionhack2023_group.jpg';
-import {FaInstagram, FaRegCalendarAlt} from 'react-icons/fa';
+import "./App.css";
+import Navbar from "./components/Navbar.js";
+import grouppic from "./images/lionhack2023_group.jpg";
+import { FaInstagram, FaRegCalendarAlt } from "react-icons/fa";
 
 const tokenKey = process.env.REACT_APP_CRYPTOPANIC_APIKEY;
-const URL = "https://cryptopanic.com/api/v1/posts/?auth_token=" + tokenKey + "&public=true";
+const URL =
+  "https://cryptopanic.com/api/v1/posts/?auth_token=" +
+  tokenKey +
+  "&public=true";
 
 function App() {
-
   // const [results, setResults] = useState(null);
 
-  useEffect(() => {
-    fetch(URL)
-      .then(res => console.log(res))
-      .then(data => console.log(data));
-      
-  }, []);
+  // useEffect(() => {
+  //   fetch(URL)
+  //     .then(res => console.log(res))
+  //     .then(data => console.log(data));
+
+  // }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar/>
+        <Navbar />
       </header>
       <div className="hero-wrap">
         <div className="hero">
@@ -37,16 +38,29 @@ function App() {
               <li>Dennis: Officer</li>
             </ul>
             <h3>Current Members</h3>
-            
           </div>
           <div className="middle-hero">
-            <img style={{width: "100%", margin: "1rem"}} src={grouppic} alt="group of umn blockchain at lionhack 2023"/>
+            <img
+              style={{ width: "100%", margin: "1rem" }}
+              src={grouppic}
+              alt="group of umn blockchain at lionhack 2023"
+            />
             <div className="hero-socials">
-              <a style={{color: 'black'}} href="https://www.instagram.com/umn_blockchain"><FaInstagram className="hero-icons"/></a>
+              <a
+                style={{ color: "black" }}
+                href="https://www.instagram.com/umn_blockchain"
+              >
+                <FaInstagram className="hero-icons" />
+              </a>
               <a href="https://discord.com/invite/EeVuh24q8E">
                 <button id="discord-button">Join our discord</button>
               </a>
-              <a style={{color: 'black'}} href="https://discord.gg/HYxB7hkrub"><FaRegCalendarAlt className="hero-icons"/></a>
+              <a
+                style={{ color: "black" }}
+                href="https://discord.gg/HYxB7hkrub"
+              >
+                <FaRegCalendarAlt className="hero-icons" />
+              </a>
             </div>
           </div>
           <div className="right-hero">
@@ -55,26 +69,31 @@ function App() {
           </div>
         </div>
         <div class="hero-corner-name">
-            <h1>University Of Minnesota Blockchain Club</h1>
+          <h1>University Of Minnesota Blockchain Club</h1>
         </div>
       </div>
       <div className="about-us" id="aboutUs">
         <h2>Why Blockchain</h2>
-        <h2 style={{fontSize: "2rem", marginBottom: "1rem"}}>More About Our Club</h2>
+        <h2 style={{ fontSize: "2rem", marginBottom: "1rem" }}>
+          More About Our Club
+        </h2>
         <div className="card-wrap">
           <div className="card">
             <p>
-              Blockchain is an emerging field with over 300 million people involved in either investing or developing
+              Blockchain is an emerging field with over 300 million people
+              involved in either investing or developing
             </p>
           </div>
           <div className="card">
             <p>
-              Great way to network and meet people in the web3 industry as well software engineers, lawyers, economists, artists, and more
+              Great way to network and meet people in the web3 industry as well
+              software engineers, lawyers, economists, artists, and more
             </p>
           </div>
           <div className="card">
             <p>
-              We meet every wednesday from 7pm-8pm and will also have free food. We teach terms and discuss with each other every meeting
+              We meet every wednesday from 7pm-8pm and will also have free food.
+              We teach terms and discuss with each other every meeting
             </p>
           </div>
         </div>
