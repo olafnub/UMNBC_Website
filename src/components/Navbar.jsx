8ea8d69bf603d10758a6661539609549cc3f6acb
MyManAy <66086069+MyManAy@users.noreply.github.com>
2023-12-20 16:53:29 -0600

    Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import CoinTicker from "./CoinTicker";
+import { useEffect, useState } from "react";
+import "./Navbar.css";
+import autoFetch from "../utils/autoFetch";
+
+function Navbar() {
+  const [coinData, setCoinData] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const coinData = await autoFetch("/coin-market/api");
+      setCoinData(coinData?.data);
+    })();
+  }, []);
+
+  return (
+    <div className="navbar">
+      <h3>
+        <a href="/" className="navAhref">
+          UMNBC
+        </a>
+      </h3>
+      <CoinTicker coinData={coinData} />
+      <nav>
+        <ul className="navUl">
+          <li>
+            <a href="#aboutUs" className="navAhref">
+              About us
+            </a>
+          </li>
+          <li>
+            <a href="/#" className="navAhref">
+              Resources
+            </a>
+          </li>
+          <li>
+            <a href="/#" className="navAhref">
+              Alumini
+            </a>
+          </li>
+          <li>
+            <a href="/#" className="navAhref">
+              Events
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+export default Navbar;
