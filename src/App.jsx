1556df164703864c2e1e30926e0755e94d2e563e
olafnub <googlhoogle@gmail.com>
2024-01-02 16:49:47 -0600

    Meet our team for the mac design

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,48 @@ function App() {
           <WeOffer />
         </div>
         <div className="our-team page-wrap" id="ourTeam">
-          <h2 style={{ fontSize: "2rem", marginBottom: "1rem"}}>Meet our team</h2>
+          <h2 style={{ fontSize: "2rem", marginBottom: "1rem"}}>Current Officers</h2>
+          <div className="officer-wrap">
+            <div className="officer-box">
+              <img src="https://oh.imgix.net/wp-content/uploads/2022/01/pudgy-penguin-3.jpg?auto=format%2Ccompress&ixlib=php-3.3.0&w=400"></img>
+              <h4>Jorgen</h4>
+              <p>Co President</p>
+            </div>
+            <div className="officer-box">
+              <img src="https://i.redd.it/pc6kk3p0rjf81.png"></img>
+                <h4>Andy</h4>
+                <p>Co President</p>
+            </div>
+            <div className="officer-box">
+              <img src="https://cdnb.artstation.com/p/assets/images/images/048/075/205/large/imdmitreey-40.jpg?1649153616"></img>
+              <h4>Anthony</h4>
+              <p>Social Media</p>
+            </div>
+            <div className="officer-box">
+              <img src="https://www.artnews.com/wp-content/uploads/2021/08/BAYC-8746.png"></img>
+              <h4>Anjali</h4>
+              <p>Social Media</p>
+            </div>
+            <div className="officer-box">
+              <img src="https://hellopartner.com/content/images/size/w1600/uz4FXC4Ws_hBBlHGegO5-FkwU9TFA1973dYtW2qZfbJXE-XgRgVy3I8frM-P2HLhBmmRgtHdUL9sK9aIr4ZjF2u8PIqqrZSO6hRBIfk-MNyHksdB2sMWGntaHAx5kHOGTa_ZcNqG.png"></img>
+              <h4>Nithin</h4>
+              <p>Web Developer</p>
+            </div>
+            <div className="officer-box">
+              <img src="https://cdnb.artstation.com/p/assets/images/images/048/075/227/large/imdmitreey-31.jpg?1649153670"></img>
+              <h4>Ahmed</h4>
+              <p>Officer</p>
+            </div>
+            <div className="officer-box">
+              <img src="https://oh.imgix.net/wp-content/uploads/2022/01/pudgy-penguin-4.jpg?auto=format%2Ccompress&ixlib=php-3.3.0&w=400"></img>
+              <h4>Jackie</h4>
+              <p>Officer</p>
+            </div>
+          </div>
+          <div id="nft-disclosure">
+            <p>we do not own any of the NFTs, all belong to the original owners | layout idea from <a className="color-red" href="https://www.cornellblockchain.org/">Cornell Blockchain</a></p>
+            {/* We could update with our own NFTs once we  */}
+          </div>
         </div>
       </div>
     </Layout>
